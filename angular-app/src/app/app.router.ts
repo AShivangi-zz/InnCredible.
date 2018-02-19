27101a1cb9639768b3bf2f069783926b374689ff
avanibhatnagar <avanibhatnagar07@gmail.com>
2018-02-19 15:55:16 -0800

    errors

--- angular-app/src/app/app.router.ts
+++ angular-app/src/app/app.router.ts
@@ -8,7 +8,7 @@ import {AuthGuard} from './auth.service';
 import {HomeComponent} from './home/home.component';
 
 export const router: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path :'home', component: HomeComponent, canActivate: [AuthGuard]}
