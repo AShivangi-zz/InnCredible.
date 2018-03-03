f4e4eedcb9b25b1d9ad6217c6be4377aac220366
Devli12 <devisukha12@gmail.com>
2018-03-03 13:15:32 -0800

    Update app.router.ts

--- angular-app/src/app/app.router.ts
+++ angular-app/src/app/app.router.ts
@@ -1,17 +1,24 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
+import { itemsListComponent } from './items/items-list/items-list.component';
+import { ReadmePageComponent } from './ui/readme-page/readme-page.component';
+import { NotesListComponent } from './notes/notes-list/notes-list.component';
+
+//import {UserFormComponent } from './ui/user-form/user-form.component';
+
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {AuthGuard} from './auth.service';
 import {HomeComponent} from './home/home.component';
 import {ProfileComponent} from './profile/profile.component';
+import {UserFormComponent} from './user-form/user-form.component';
 
 export const router: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
+    {path: 'register', component: UserFormComponent},
     {path: 'home', component: HomeComponent},
     {path: 'profile', component: ProfileComponent}
 ]
