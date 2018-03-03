a4b9eb7f6597b45f68d9f3ab967c3c09500c558f
Devli12 <devisukha12@gmail.com>
2018-03-03 13:15:01 -0800

    Update auth.service.ts

--- angular-app/src/app/auth.service.ts
+++ angular-app/src/app/auth.service.ts
@@ -1,12 +1,78 @@
 import {CanActivate, Router} from '@angular/router';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
+import * as firebase from 'firebase/app';
+import { AngularFirestore, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
 
+interface User {
+  uid: string;
+  email: string;
+  photoURL: string;
+  rewordPoints: string;
+
+}
+
 @Injectable()
+
+export class AuthService {
+  user: Observable<User>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private router: Router,
+              private notify: NotifyService) {
+
+        this.user = this.afAuth.authState
+          .switchMap(user => {
+            if(user) {
+              return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
+            } else {
+              return Observable.of(null)
+            }
+          })
+        }
+
+        emailSignUp(email: string, password: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        return this.setUserDoc(user) // create initial user document
+      })
+      .catch(error => this.handleError(error) );
+  }
+
+  // Update properties on the user document
+  updateUser(user: User, data: any) {
+    return this.afs.doc(`users/${user.uid}`).update(data)
+  }
+
+  // If error, console log and notify user
+  private handleError(error) {
+    console.error(error)
+    this.notify.update(error.message, 'error')
+  }
+
+  // Sets user data to firestore after succesful login
+  private setUserDoc(user) {
+
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email || null,
+      photoURL: 'https://goo.gl/Fz9nrQ',
+      rewordPoints: user.rewordPoints
+    }
+
+    return userRef.set(data)
+
+  }
+
+}
 export class AuthGuard implements CanActivate {
 
     constructor(private afAuth: AngularFireAuth, private router: Router) { }
@@ -20,5 +86,5 @@ export class AuthGuard implements CanActivate {
                     this.router.navigate(['/login']);
                 }
         });
-    } 
+    }
 }
