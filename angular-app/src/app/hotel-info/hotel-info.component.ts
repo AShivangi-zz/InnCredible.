afa9e96d4f8c45ada6aead4d66c884a28f50427c
Devli12 <devisukha12@gmail.com>
2018-03-09 08:43:41 -0800

    Update hotel-info.component.ts

--- angular-app/src/app/hotel-info/hotel-info.component.ts
+++ angular-app/src/app/hotel-info/hotel-info.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {hotel-info} from "../services/hotel-info";
+//import {AngularFire} from "angularfire5";
 
 @Component({
   selector: 'app-hotel-info',
   templateUrl: './hotel-info.component.html',
   styleUrls: ['./hotel-info.component.scss']
 })
-export class HotelInfoComponent implements OnInit {
 
-  constructor() { }
+export class HotelInfoComponent implements OnInit {
+  toolbarTitle = 'CP2';
+  amenities: result[];
+
+  constructor(private af : AngularFire) {
+   }
 
   ngOnInit() {
+     this.af.database.list('/amenities').subscribe(users => { 
+     	this.amenities =  amenities
+     });
+      
   }
 
 }
