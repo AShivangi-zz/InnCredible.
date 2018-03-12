65499ef3e8fc7ac080ff8fdaccbd7b945fcc485d
lwbarrett <lance.barrett@sjsu.edu>
2018-03-12 00:44:10 -0700

    added reservation

--- angular-app/src/app/booking/booking.component.ts
+++ angular-app/src/app/booking/booking.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.scss']
+  styleUrls: ['./booking.component.scss'],
 })
 export class BookingComponent implements OnInit {
 
-
   constructor() { }
-
   ngOnInit() {
   }
 
