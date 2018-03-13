65d896f40ea2da0d7318e38fa21c8870ea499db5
Devli12 <devisukha12@gmail.com>
2018-03-12 17:42:11 -0700

    Update hotel-info.component.ts

--- angular-app/src/app/hotel-info/hotel-info.component.ts
+++ angular-app/src/app/hotel-info/hotel-info.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelInfo } from "../services/hotel-info";
 
 @Component({
   selector: 'app-hotel-info',
   templateUrl: './hotel-info.component.html',
   styleUrls: ['./hotel-info.component.scss']
 })
+
 export class HotelInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private hotelInfo: HotelInfo) {
+    // this.hotelInfo.setHotelId('0');
+
+   }
 
   ngOnInit() {
+
   }
 
-}
+  }
+
