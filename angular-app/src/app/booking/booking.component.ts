9ac8555d142ec466b7e043d6df55c236af29af77
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2018-03-18 18:16:20 -0700

    Can now charge. Kill me.

--- angular-app/src/app/booking/booking.component.ts
+++ angular-app/src/app/booking/booking.component.ts
@@ -1,5 +1,6 @@
+import { Charge } from './charge';
 import { Component, OnInit } from '@angular/core';
-import { Http, Headers} from '@angular/http';
+import { Http, Headers, URLSearchParams} from '@angular/http';
 
 // import { UserProfileService } from '../services/profile.service';
 declare function createCharge(token);
@@ -28,20 +29,23 @@ export class BookingComponent implements OnInit {
     }, (status: number, response: any) => {
       if (status === 200) {
         this.message = `Success! Card token ${response.card.id}.`;
-        this.createCharge(response.id);
+        let data = new URLSearchParams();
+        data.append('card', response.id)
+        data.append('currency', 'usd')
+        data.append('amount', '1000')
+        this.createCharge(data);
       } else {
         this.message = response.error.message;
       }
     });
   }
 
-  createCharge(token: string) {
+  createCharge(data) {
     const headers = new Headers({
       'Authorization': 'Bearer sk_test_S62sR6QYYNM9biuvTdPZOH7V', 
-      'source': token, 
-      'amount': 1000}
-    );
-    this.http.post('https://api.stripe.com/v1/charges', {}, {headers: headers})
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+    this.http.post('https://api.stripe.com/v1/charges', data, {headers: headers})
       .subscribe(resp => {console.log(resp);})
   }
 
