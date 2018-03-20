579386ae4551bc76cd5e49478d1a0587fcd05b96
lwbarrett <lance.barrett@sjsu.edu>
2018-03-19 19:50:23 -0700

    Updated Rooms field to pre-calculate.

--- angular-app/src/app/booking/reservation/reservation.component.ts
+++ angular-app/src/app/booking/reservation/reservation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm} from '@angular/forms';
+import { NgForm, FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { ReservationService } from '../shared/reservation.service';
 
 @Component({
@@ -11,15 +11,28 @@ import { ReservationService } from '../shared/reservation.service';
 
 export class ReservationComponent implements OnInit {
   guestOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  bedOptions = [1, 2, 3, 4];
+  resvForm: FormGroup;
 
-  constructor(private reservationService: ReservationService) { }
+
+  constructor(private reservationService: ReservationService, private fb: FormBuilder) { }
 
   ngOnInit() {
-
+    this.resvForm = this.createGuestForm();
+    const guests = this.resvForm.get('guests');
+    const rooms = this.resvForm.get('rooms');
+    guests.valueChanges.subscribe(val => {
+      rooms.setValue(Math.ceil(val / 2));
+    });
   }
 
   onSubmit(reservationForm: NgForm) {
     this.reservationService.insertReservation(reservationForm.value);
+  }
+
+  createGuestForm() {
+      return this.fb.group({
+        guests: 1,
+        rooms:  0
+      });
   }
 }
