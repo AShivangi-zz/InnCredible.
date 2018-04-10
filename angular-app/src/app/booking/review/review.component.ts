0d3211a94a588ab23f1fe73718192226b8a3c192
CrystleJ <CrystleJohnson@Howdy.local>
2018-04-09 21:06:09 -0700

    Format for date on review page

--- angular-app/src/app/booking/review/review.component.ts
+++ angular-app/src/app/booking/review/review.component.ts
@@ -63,4 +63,19 @@ export class ReviewComponent implements OnInit {
     }
     this.userProfileService.awardRewardPoints(this.roomCharge());
   }
+
+  formatDate(date) {
+    if(date === null) {
+      return "";
+    }
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
 }
